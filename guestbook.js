28311f8e049d1841fea6a7e36adbbbe6140cb585
ArthasTsang <twyarthas@yahoo.com.hk>
2022-05-30 00:03:25 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `https://5f34328a.au-syd.apigw.appdomain.cloud/guestbook/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
